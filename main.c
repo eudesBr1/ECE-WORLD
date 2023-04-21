58c61d7bdf7b7f3f15e005c25742af87267c1939
eudesBr1 <131437101+eudesBr1@users.noreply.github.com>
2023-04-21 12:32:49 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include "allegro.h"
+
+typedef struct player{
+    int x,y,ticket,points;
+    char* name;
+}player;
+
+void initEcran(){
+    set_color_depth(desktop_color_depth());
+    if ((set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0)) != 0) {
+        allegro_message("Pb de mode graphique");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(){
+    allegro_init();
+    initEcran();
+    install_keyboard();
+    install_mouse();
+    BITMAP *carte;
+    carte = load_bitmap("ville_menu.bmp",NULL);
+    if (!carte){
+        allegro_message("Pb de l'image chargee");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    //stretch_blit(carte,screen,0,0,carte->w,carte->h,0,0,screen->w,screen->h);
+    while (!key[KEY_ESC])
+        blit(carte,screen,0,0,0,0,screen->w, screen->w);
+
+    allegro_exit();
+    return 1;
+}
+END_OF_MAIN()
