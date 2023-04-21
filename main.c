7669032ba56b0c1156a19acd922467c8bb54dba3
eudesBr1 <eudesb@hotmail.com>
2023-04-21 16:59:05 +0200

    deplacement personnage

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include "allegro.h"
 #include "stdio.h"
-
+#define vitesse 1.25
 typedef struct player{
     int x,y,ticket,points;
     char* name;
@@ -11,6 +11,7 @@ typedef struct player{
     BITMAP *droite[3];
     BITMAP *gauche[3];
     int position[3];
+    int animation;
 }t_player;
 
 
@@ -30,8 +31,8 @@ void initEcran(){
 int main(){
     allegro_init();
     t_player goku;
-    goku.x=10;
-    goku.y=10;
+    goku.x=100;
+    goku.y=100;
     initEcran();
     install_keyboard();
     install_mouse();
@@ -46,10 +47,18 @@ int main(){
         allegro_exit();
         exit(EXIT_FAILURE);
     }
-    goku.haut[0] = create_sub_bitmap(GOKUTEMPLATE,0,149,32,48);
-    goku.gauche[0] = create_sub_bitmap(GOKUTEMPLATE,0,46,32,49);
-    goku.droite[0] = create_sub_bitmap(GOKUTEMPLATE,0,96,32,49);
-    goku.bas[0] = create_sub_bitmap(GOKUTEMPLATE,0,0,32,49);
+    for (int i = 0; i < 3; ++i) {
+        goku.haut[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,149,32,48);
+    }
+    for (int i = 0; i < 3; ++i) {
+        goku.gauche[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,46,32,49);
+    }
+    for (int i = 0; i < 3; ++i) {
+        goku.droite[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,96,32,49);
+    }
+    for (int i = 0; i < 3; ++i) {
+        goku.bas[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,0,32,48);
+    }
     // Créer un sous-bitmap pour extraire le premier sprite
     BITMAP *spriteTemplate = create_sub_bitmap(GOKUTEMPLATE, 0, 0, 32, 48);
 
@@ -66,29 +75,34 @@ int main(){
         exit(EXIT_FAILURE);
     }
     //stretch_blit(carte,screen,0,0,carte->w,carte->h,0,0,screen->w,screen->h);
+    goku.animation=0;
     while (!key[KEY_ESC]){
         blit(buffer,screen,0,0,0,0,screen->w, screen->h);
         blit(carte,buffer,goku.x,goku.y,0,0,screen->w,screen->h);
         if(key[KEY_LEFT]){
-            goku.x -= 2;
-            draw_sprite(buffer,goku.gauche[0],goku.x,goku.y);
+            goku.x -= vitesse;
+            draw_sprite(buffer,goku.gauche[goku.animation/10],goku.x,goku.y);
         }
         if(key[KEY_RIGHT]){
-            goku.x += 2;
-            draw_sprite(buffer,goku.droite[0],goku.x,goku.y);
+            goku.x += vitesse;
+            draw_sprite(buffer,goku.droite[goku.animation/10],goku.x,goku.y);
         }
         if(key[KEY_UP]) {
-            goku.y -= 2;
-            draw_sprite(buffer,goku.haut[0],goku.x,goku.y);
+            goku.y -= vitesse;
+            draw_sprite(buffer,goku.haut[goku.animation/10],goku.x,goku.y);
         }
         if(key[KEY_DOWN]){
-            goku.y += 2;
-            draw_sprite(buffer,goku.bas[0],goku.x,goku.y);
+            goku.y += vitesse*3;
+            draw_sprite(buffer,goku.bas[goku.animation/10],goku.x,goku.y);
         }
         rest(10);
+        goku.animation++;
+        if (goku.animation==30){
+            goku.animation=0;
+        }
+     //   printf("%d",goku.animation);
         }
     allegro_exit();
     return 1;
-
 }
 END_OF_MAIN()
