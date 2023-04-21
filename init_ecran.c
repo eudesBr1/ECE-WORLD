a1fe254c8228a896fbc7cdd50f07a0bf3c2f20e2
AntoineLal <antoine.nl.lallement@gmail.com>
2023-04-21 22:27:45 +0200

    début du jeux du ballon qui fonctionne normalemnt + séparation en sous programmes dans différents bibli

---
+++ init_ecran.c
@@ -0,0 +1,14 @@
+//
+// Created by antoi on 21/04/2023.
+//
+
+#include "Mabibli.h"
+
+void initEcran(){
+    set_color_depth(desktop_color_depth());
+    if ((set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0)) != 0) {
+        allegro_message("Pb de mode graphique");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+}
