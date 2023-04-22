857079b85fade2b1d12630ea1d0be46a825c5558
eudesBr1 <eudesb@hotmail.com>
2023-04-22 19:54:06 +0200

    creation animation personage en SS fonction

--- main.c
+++ main.c
@@ -3,52 +3,22 @@
 int main(){
     allegro_init();
     t_player goku;
-    goku.x=100;
-    goku.y=100;
     initEcran();
     install_keyboard();
     install_mouse();
     BITMAP *carte;
-    BITMAP *GOKUTEMPLATE;
-    BITMAP *GOKU;
     srand(time(NULL));
     BITMAP *buffer= create_bitmap(screen->w,screen->h);
     clear(buffer);
-    GOKUTEMPLATE = load_bitmap("../images/gokuSprite.bmp",NULL);
-    if (!GOKUTEMPLATE){
-        allegro_message("Pb de l'image chargee");
-        allegro_exit();
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < 3; ++i) {
-        goku.haut[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,149,32,48);
-    }
-    for (int i = 0; i < 3; ++i) {
-        goku.gauche[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,46,32,49);
-    }
-    for (int i = 0; i < 3; ++i) {
-        goku.droite[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,96,32,49);
-    }
-    for (int i = 0; i < 3; ++i) {
-        goku.bas[i] = create_sub_bitmap(GOKUTEMPLATE,32*i,0,32,48);
-    }
-    // Créer un sous-bitmap pour extraire le premier sprite
-    BITMAP *spriteTemplate = create_sub_bitmap(GOKUTEMPLATE, 0, 0, 32, 48);
+    creationPersonnage(&goku);
 
-    // Créer un nouveau bitmap pour votre personnage
-    GOKU = create_bitmap(spriteTemplate->w, spriteTemplate->h);
-
-    // Copier le sprite extrait dans votre nouveau bitmap
-    clear_bitmap(GOKU);
-    blit(spriteTemplate, GOKU, 0, 0, 0, 0, spriteTemplate->w, spriteTemplate->h);
     carte = load_bitmap("../images/map_pokemon.bmp",NULL);
     if (!carte){
         allegro_message("Pb de l'image chargee");
         allegro_exit();
         exit(EXIT_FAILURE);
     }
-    //stretch_blit(carte,screen,0,0,carte->w,carte->h,0,0,screen->w,screen->h);
-    goku.animation=0;
+
     while (!key[KEY_ESC]){
         blit(buffer,screen,0,0,0,0,screen->w, screen->h);
         blit(carte,buffer,goku.x,goku.y,0,0,screen->w,screen->h);
@@ -77,4 +47,3 @@ int main(){
     return 1;
 }
 END_OF_MAIN()
-
