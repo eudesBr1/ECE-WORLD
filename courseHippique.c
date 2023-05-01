59eac00ca2395ecb01fec64599881f64df9d4edd
eudesBr1 <eudesb@hotmail.com>
2023-05-01 13:03:57 +0200

    course Hippique ajout Chevaux

--- courseHippique.c
+++ courseHippique.c
@@ -4,17 +4,22 @@
 #include "Mabibli.h"
 
 void courseHippique(t_player *player){
-    while (!key[KEY_ESC]){
-        BITMAP *buffer;
-        buffer = create_bitmap(screen->w,screen->h);
-        BITMAP *fondCourseHippique;
-        fondCourseHippique = load_bitmap("../images/courseHippique.bmp",NULL);
-        if (!fondCourseHippique){
-            allegro_message("Pb de l'image courseHippique.bmp");
-            allegro_exit();
-            exit(EXIT_FAILURE);
-        }
-        stretch_blit(fondCourseHippique, buffer, 0, 0, fondCourseHippique->w, fondCourseHippique->h, 0, 0, fondCourseHippique->w, screen->h);
+    int coordonneesPiste=0;
+    BITMAP *buffer;
+    buffer = create_bitmap(screen->w,screen->h);
+    BITMAP *fondCourseHippique;
+    fondCourseHippique = load_bitmap("../images/courseHippique.bmp",NULL);
+    if (!fondCourseHippique){
+        allegro_message("Pb de l'image courseHippique.bmp");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    while (!key[KEY_ESC] && coordonneesPiste<1300){
+        stretch_blit(fondCourseHippique, buffer, coordonneesPiste, 0, fondCourseHippique->w,fondCourseHippique->h,0,0,fondCourseHippique->w,buffer->h);
         blit(buffer,screen,0,0,0,0,screen->w,screen->h);
+        rest(1);
+        coordonneesPiste +=1;
+        printf("Coordonnes piste %d\n",coordonneesPiste);
+        printf("taille screen %d\n",buffer->w);
     }
 }
