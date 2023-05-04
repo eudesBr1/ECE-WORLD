3271a00ff40d6945b4ec47c1c97672c79fc9ba92
AntoineLal <antoine.nl.lallement@gmail.com>
2023-05-04 22:43:12 +0200

    modif pour que ce soit compatible

--- affichageMenuVille.c
+++ affichageMenuVille.c
@@ -6,60 +6,61 @@
 #define hauteurPersonnage1 45
 #define largeurPersonnage2 40
 #define hauteurPersonnage2 55
-void affichageVille(t_player *player,t_player *player2){
+void affichageVille(t_player *players){
     BITMAP *buffer= create_bitmap(screen->w,screen->h);
     clear(buffer);
     BITMAP *carte;
     /// charge carte ville
+    printf("test\n");
     carte = load_bitmap("../images/map_pokemon.bmp",NULL);
     if (!carte){
         allegro_message("Pb de l'image chargee");
         allegro_exit();
         exit(EXIT_FAILURE);
     }
-    stretch_sprite(screen,player2->haut[player2->animation/10 %qtAnim],player2->x,player2->y,largeurPersonnage2,hauteurPersonnage2);
+    printf("test\n");
+    stretch_sprite(screen,players[1].haut[players[1].animation/10 %qtAnim],players[1].x,players[1].y,largeurPersonnage2,hauteurPersonnage2);
+    printf("test\n");
     while (!key[KEY_ESC]){
+        printf("test\n");
         blit(buffer,screen,0,0,0,0,screen->w, screen->h);
-        blit(carte,buffer,player->x,player->y,0,0,screen->w,screen->h);
-        mouvementPersonnageZQSD(player);
-        mouvementPersonnageFleche(player2);
+        blit(carte,buffer,players[0].x,players[0].y,0,0,screen->w,screen->h);
+        printf("test\n");
+        mouvementPersonnageZQSD(&players[0]);
+        printf("test\n");
+        mouvementPersonnageFleche(&players[1]);
+        printf("test\n");
+
         // affiche personnage sur carte
-        if (player->position==0){
-            stretch_sprite(buffer,player->haut[player->animation/10 %qtAnim],player->x,player->y,largeurPersonnage1,hauteurPersonnage1);
+        for (int i = 0; i < players[0].nbJoueurs; i++) {
+            printf("test\n");
+            if (players[i].position == 0) {
+                stretch_sprite(buffer, players[i].haut[players[i].animation / 10 % qtAnim], players[i].x, players[i].y,
+                               largeurPersonnage2, hauteurPersonnage2);
+            }
+            if (players[i].position == 1) {
+                stretch_sprite(buffer, players[i].droite[players[i].animation / 10 % qtAnim], players[i].x,
+                               players[i].y, largeurPersonnage2, hauteurPersonnage2);
+            }
+            if (players[i].position == 2) {
+                stretch_sprite(buffer, players[i].bas[players[i].animation / 10 % qtAnim], players[i].x, players[i].y,
+                               largeurPersonnage2, hauteurPersonnage2);
+            }
+            if (players[i].position == 3) {
+                stretch_sprite(buffer, players[i].gauche[players[i].animation / 10 % qtAnim], players[i].x,
+                               players[i].y, largeurPersonnage2, hauteurPersonnage2);
+            }
         }
-        if (player->position==1){
-            stretch_sprite(buffer,player->droite[player->animation/10 %qtAnim],player->x,player->y,largeurPersonnage1,hauteurPersonnage1);
-        }
-        if (player->position==2){
-            stretch_sprite(buffer,player->bas[player->animation/10 %qtAnim],player->x,player->y,largeurPersonnage1,hauteurPersonnage1);
-        }
-        if (player->position==3){
-            stretch_sprite(buffer,player->gauche[player->animation/10 %qtAnim],player->x,player->y,largeurPersonnage1,hauteurPersonnage1);
-        }
-        ///affichage joueur 2 ville
-        if (player2->position==0){
-            stretch_sprite(buffer,player2->haut[player2->animation/10 %qtAnim],player2->x,player2->y,largeurPersonnage2,hauteurPersonnage2);
-        }
-        if (player2->position==1){
-            stretch_sprite(buffer,player2->droite[player2->animation/10 %qtAnim],player2->x,player2->y,largeurPersonnage2,hauteurPersonnage2);
-        }
-        if (player2->position==2){
-            stretch_sprite(buffer,player2->bas[player2->animation/10 %qtAnim],player2->x,player2->y,largeurPersonnage2,hauteurPersonnage2);
-        }
-        if (player2->position==3){
-            stretch_sprite(buffer,player2->gauche[player2->animation/10 %qtAnim],player2->x,player2->y,largeurPersonnage2,hauteurPersonnage2);
-        }
-        printf("%d\n",player->animation %qtAnim);
         rest(20);
-        if (key[KEY_SPACE]) game_GUITARE(player);
-        if (key[KEY_H]) courseHippique(player);
+        if (key[KEY_SPACE]) game_GUITARE(&players[0]);
+        if (key[KEY_H]) courseHippique(&players[0]);
     }
     destroy_bitmap(buffer);
     destroy_bitmap(carte);
     for (int i = 0; i < 4; ++i) {
-        destroy_bitmap(player->droite[i]);
-        destroy_bitmap(player->gauche[i]);
-        destroy_bitmap(player->haut[i]);
-        destroy_bitmap(player->bas[i]);
+        destroy_bitmap(players[1].droite[i]);
+        destroy_bitmap(players[1].gauche[i]);
+        destroy_bitmap(players[1].haut[i]);
+        destroy_bitmap(players[1].bas[i]);
     }
 }
