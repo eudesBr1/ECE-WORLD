32aef7183f18174a8173866ca8326c4a34920462
eudesBr1 <eudesb@hotmail.com>
2023-05-10 18:30:05 +0200

    gardien lucarne

--- penalty.c
+++ penalty.c
@@ -29,7 +29,7 @@ void penalty(t_player *player){
     t_ballonFoot ballon;
     ballon.tailleBallon = 100;
     ballon.positionX=screen->w/2-ballon.tailleBallon/2;
-    ballon.positionY=screen->h-200;
+    ballon.positionY=screen->h-150;
     for (int i = 0; i <= 2; ++i) {
         ballon.effet[i]= create_sub_bitmap(templateBallon,500*i+30,30,500,500);
     }
@@ -40,18 +40,22 @@ void penalty(t_player *player){
     t_gardien gardien;
     gardien.hauteur = 210;
     gardien.largeur = 115;
+    gardien.positionX=screen->w/2-gardien.largeur/2;
+    gardien.positionY=screen->h/2;
     for (int i = 0; i <= 2; ++i) {
         gardien.position[i] = create_sub_bitmap(templateGardien,350*i+40,40,350,470);
     }
     for (int i = 3; i <= 5; ++i) {
         gardien.position[i] = create_sub_bitmap(templateGardien,350*i+40,40,350,470);
-    }
+
+
     ///commence le duel rien ne se passe
     while (!key[KEY_ESC] && !key[KEY_E]){
+        ballon.tailleBallon=100;
         clear(buffer);
         stretch_blit(fondStade,buffer,0,0,fondStade->w,fondStade->h,0,0,screen->w,screen->h);
         stretch_sprite(buffer,ballon.effet[0],ballon.positionX,ballon.positionY,ballon.tailleBallon,ballon.tailleBallon);
-        stretch_sprite(buffer,gardien.position[(b/30)%3],screen->w/2-gardien.largeur/2,screen->h/2-50,gardien.largeur,gardien.hauteur);
+        stretch_sprite(buffer,gardien.position[(b/30)%3],gardien.positionX,gardien.positionY,gardien.largeur,gardien.hauteur);
         show_mouse(buffer);
         blit(buffer,screen,0,0,0,0,screen->w,screen->h);
         rest(1);
@@ -59,19 +63,22 @@ void penalty(t_player *player){
             ///le ballon tourne et se deplace
            if (mouse_x > 690 && mouse_x<873 && mouse_y>530 && mouse_y<640){
                b=0;
-               //monte 1.6 fois plus vite que va vers la gauche
-               while (!key[KEY_ESC]){
+               //monte 1.6 fois plus vite que va vers la gauche 250 gauche 400 haut
+               while (!key[KEY_ESC] && ballon.positionX > 700){
                    clear(buffer);
                    stretch_blit(fondStade,buffer,0,0,fondStade->w,fondStade->h,0,0,screen->w,screen->h);
-                   stretch_sprite(buffer,gardien.position[(b/30)%3],screen->w/2-gardien.largeur/2,screen->h/2,gardien.largeur,gardien.hauteur);
-                   stretch_sprite(buffer,ballon.effet[(b/10)%3],screen->w/2-ballon.tailleBallon/2,screen->h-200-b,ballon.tailleBallon-b/30,ballon.tailleBallon-b/30);
+                   stretch_sprite(buffer,gardien.position[(b/30)%3],gardien.positionX,gardien.positionY,gardien.largeur,gardien.hauteur);
+                   stretch_sprite(buffer,ballon.effet[(b/10)%3],ballon.positionX,ballon.positionY,ballon.tailleBallon,ballon.tailleBallon);
                    blit(buffer,screen,0,0,0,0,screen->w,screen->h);
                    rest(1);
+                   ballon.positionX-=250/100;
+                   ballon.positionY-=400/100;
+                   ballon.tailleBallon-=1;
                    b++;
                }
            }
         }
         b++;
     }
-
+  }
 }
