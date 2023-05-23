12930d93b062c8a8865645631b073c8607c227b0
tony_sopranos <malik.hassane@gmail.com>
2023-05-23 10:48:04 +0200

    marios

--- Mabibli.h
+++ Mabibli.h
@@ -98,6 +98,8 @@ typedef struct tuiles{
     int pressed[4][8];
 }t_tuiles;
 
+
+void game_marios();
 void initEcran();
 void gameInit(t_player players[4]);
 void ballon(t_player *player);
@@ -109,7 +111,7 @@ t_player creationPersonnageGoku(t_player *player);
 void affichageVille(t_player players[4]);
 void courseHippique(t_player *player);
 int debutCourse(t_player *player);
-void jackpot_game();
+void jackpot_game(t_player players[4],int numJoueur);
 void game_PONG(t_player player[4],int numJoueur);
 int coinFlip();
 void penalty(t_player *player);
